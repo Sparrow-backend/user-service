c974d791de3dc590709f4444e9e232adeb81e6c4
sliitguy <it23259416@my.sliit.lk>
2025-10-02 15:55:38 +0530

    updated minor changes

--- server.js
+++ server.js
@@ -6,31 +6,31 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const PORT = process.env.PORT || 8003
-const MONGODB_URI = process.env.MONGODB_URI
-
-const server = http.createServer(app)
 
 mongoose.connection.once('open', () => {
-    console.log('MongoDB is ready!')
+    console.log("MongoDB is ready!")
 })
 
 mongoose.connection.on('error', () => {
-    console.log("Error in connecting with MongoDB!")
+    console.error('Error in connecting with MongoDB')
 })
+
+const server = http.createServer(app)
 
 async function createServer() {
     try {
-
-        mongoose.connect(MONGODB_URI)
-        console.log('Connected to MongoDB')
-
+        
         server.listen(PORT, () => {
-            console.log(`User Service: Listening on port ${PORT}`)
+            console.log(`Server listening on port ${PORT}..`)
         })
 
+        
     } catch(err) {
-        console.error("User service: Internal service error", err)
+        console.error('Internal server error: ', err)
+        process.exit(1)
     }
 }
 
 createServer()
+
+
